01c20934187501fdf9a1335dd8d917842fe4031e
Akshaykrishna4216 <129931476+Akshaykrishna4216@users.noreply.github.com>
2023-05-10 17:19:45 +0530

    Add files via upload

---
+++ MVCDemoApp/MVCDemoApp/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿namespace MVCDemoApp.Models
+{
+    public class Customer
+    {
+        public int CustId { get; set; }
+        public string Name { get; set; }
+        public decimal Balance { get; set; }
+
+        public Customer()
+        {
+
+        }
+    }
+}
