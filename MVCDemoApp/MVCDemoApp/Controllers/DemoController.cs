b240bd6997fa079bab35ca2e9afe133eb11ccc6b
Akshaykrishna4216 <129931476+Akshaykrishna4216@users.noreply.github.com>
2023-05-10 17:17:31 +0530

    Add files via upload

---
+++ MVCDemoApp/MVCDemoApp/Controllers/DemoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCDemoApp.Controllers
+{
+   
+    
+    public class DemoController : Controller
+    {
+        public string Display()
+        {
+            return "MVC Core";
+        }
+            public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
